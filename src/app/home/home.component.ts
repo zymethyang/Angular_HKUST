2963dc3aebdbddb30da0c640f270d4494f932856
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-07 23:10:35 +0700

    RxJs Part 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -24,11 +24,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-    .then(dish=>this.dish=dish);
+    .subscribe(dish=>this.dish=dish);
     this.promotionservice.getFeaturedPromotion()
-    .then(promotion =>this.promotion=promotion);
-    this.leaderservice.getLeader().
-    then(leader=>this.leader=leader);
+    .subscribe(promotion =>this.promotion=promotion);
+    this.leaderservice.getLeader()
+    .subscribe(leader=>this.leader=leader);
   }
 
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { LEADERS } from '../shared/leaders';
 import { Leader } from '../shared/leader';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/delay';
+
 @Injectable()
 export class LeaderService {
   constructor() { }
-  getLeader(): Promise<Leader> {
-    return new Promise(resolve => {
-      setTimeout(() => (LEADERS.filter((leader) => leader)[3]), 2000);
-    });
+  getLeaders(): Observable<Leader[]> {
+    return Observable.of(LEADERS).delay(2000);
   }
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve => {
-      setTimeout(() => (LEADERS), 2000);
-    });
+  getLeader(): Observable<Leader> {
+    return Observable.of(LEADERS.filter((leader) => leader)[3]).delay(2000);
   }
+
 }
