71ed232210a6cd983bb61448599027876a6c6a42
Nguyen Minh Tri <zymethyang@gmail.com>
2017-09-08 11:20:33 +0700

    RxJS Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DatePipe } from '@angular/common';
-
+import 'rxjs/add/operator/switchMap';
 import { DishService } from '../services/dish.service';
 
 const DISH = {
@@ -48,25 +48,36 @@ const DISH = {
 };
 
 @Component({
-  selector: 'app-dishdetail',
-  templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+    selector: 'app-dishdetail',
+    templateUrl: './dishdetail.component.html',
+    styleUrls: ['./dishdetail.component.scss']
 })
 export class DishdetailComponent implements OnInit {
-    
-      dish: Dish;
-    
-      constructor(private dishservice: DishService,
+    dishIds: number[];
+    prev: number;
+    next: number;
+    dish: Dish;
+
+    constructor(private dishservice: DishService,
         private route: ActivatedRoute,
         private location: Location) { }
-    
-      ngOnInit() {
-        let id = +this.route.snapshot.params['id'];
-        this.dishservice.getDish(id).subscribe(dish => this.dish=dish);
-      }
-    
-      goBack(): void {
-        this.location.back();
-      }
-    
+
+    ngOnInit() {
+
+        this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+        this.route.params
+            .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
+            .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
     }
+
+    setPrevNext(dishId: number) {
+        let index = this.dishIds.indexOf(dishId);
+        this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+        this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+    }
+
+    goBack(): void {
+        this.location.back();
+    }
+
+}
